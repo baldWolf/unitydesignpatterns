41fce0a3585aee3d8d627dccc232c83444a29451
baldWolf <lowolfnest@gmail.com>
2016-10-10 22:43:56 +0800

    Renamed the script from OpObjectPool to DesObjectPool.

--- unity/Assets/Scripts/Creational/Object Pool/DesObjectPool.cs
+++ unity/Assets/Scripts/Creational/Object Pool/DesObjectPool.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OpObjectPool : MonoBehaviour {
+public class DesObjectPool : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
